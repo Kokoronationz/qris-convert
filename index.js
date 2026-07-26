dac87b2dd153bde3ef574c9041bf1b11ef959c09
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2026-07-26 20:54:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export { convertQris } from './src/converter.js';
 export { parseQris, getMerchantName, getMerchantCity, getAmount } from './src/parser.js';
 export { crc16ccitt } from './src/crc16.js';
-export { readQris } from './src/reader.js'
+export { readQR } from './src/reader.js'
 export { createQR } from './src/create.js'
